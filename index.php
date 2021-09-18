702d1046cf05ce96a9a899c048e5fab8bac6bab2
koras <kos6476@yandex.ru>
2021-09-18 20:21:21 +0300

    мои комментарии

--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <?php
+
+$folderFrom = 'c:/film';
+
+$folderTo = 'd:/films';
+
+$numFolder = 0;
+$numFile = 0;
+
+
 function start($folderA,$folderB) {
-    $folderA = A;
-    $folderB = B;
+  //  $folderA = A;
+ //   $folderB = B;
+     global $numFile, $numFolder;
+
     $dir = opendir($folderA);
-    @mkdir($folderA);
+    @mkdir($folderB);
+
     while(false !== ( $file = readdir($dir)) ) {
         if (( $file != '.' ) && ( $file != '..' )) {
             if ( is_dir($folderA . '/' . $file) ) {
                 start($folderA . '/' . $file,$folderB . '/' . $file);
-                echo count ($dir);
+              //  global $num;
+              $numFolder++;
 
             }
             else {
                 copy($folderA . '/' . $file, $folderB . '/' . $file);
-                echo count ($file);
-
-
+                $numFile++;
             }
         }
     }
     closedir($dir);
 }
 
+start($folderFrom,$folderTo);
+
+echo $numFile . " file". $numFolder;
+
 ?>
+
+
+<html></html>
