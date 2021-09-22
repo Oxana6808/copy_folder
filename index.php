fafacf405e5eb800adaf7f5cf39fc56b1ec52a80
koras <kos6476@yandex.ru>
2021-09-23 00:06:31 +0300

    поправил скрипт копирования файлов и папок Добавил свои комментарии и пояснения Добавил папки для тестирования и копирования

--- index.php
+++ index.php
@@ -1,43 +1,44 @@
 <?php
 
-$folderFrom = 'c:/film';
+// путь до папки от куда копируем
+$folderFrom = 'folderA';
 
-$folderTo = 'd:/films';
+// путь до папки куда копируем
+$folderTo = 'folderB';
 
+// количество папок скопированных
 $numFolder = 0;
+// количество файлов которые скопировали
 $numFile = 0;
-
-
-function start($folderA,$folderB) {
-  //  $folderA = A;
- //   $folderB = B;
+/**
+ * функция копирования файлов и папок
+ * Законченное решение
+ */
+function start($folderA,$folderB) { 
      global $numFile, $numFolder;
 
     $dir = opendir($folderA);
     @mkdir($folderB);
-
+    // перебираем все файлы в текущей папке
     while(false !== ( $file = readdir($dir)) ) {
         if (( $file != '.' ) && ( $file != '..' )) {
             if ( is_dir($folderA . '/' . $file) ) {
+                // если папка существует то рекурсивно её копируем
                 start($folderA . '/' . $file,$folderB . '/' . $file);
-              //  global $num;
               $numFolder++;
-
             }
             else {
+                // файл тоже копируем, но без рекурсии
                 copy($folderA . '/' . $file, $folderB . '/' . $file);
                 $numFile++;
             }
         }
     }
+    // Заканчиваем чтение папки. 
+    // Держат открытой папку плохая практика
     closedir($dir);
 }
 
+// запускаем скрипт копирования файлов
 start($folderFrom,$folderTo);
-
-echo $numFile . " file". $numFolder;
-
-?>
-
-
-<html></html>
+echo " файлы={$numFile} <br> папки={$numFolder}";
